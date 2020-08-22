4db0ba454beb91ddd3a70eef816af24572e288f7
penguin418 <penguin418@naver.com>
2020-08-23 02:38:17 +0900

    feat: Added video recording

--- background.js
+++ background.js
@@ -13,3 +13,57 @@ chrome.extension.onConnect.addListener((port) => {
     chrome.desktopCapture.chooseDesktopMedia(['screen'], onAccessApproved);
   });
 });
+
+// 캡쳐 승인됨
+function onAccessApproved(id, options) {
+  if (!id) {
+    console.log('Access Denied');
+    return;
+  }
+
+  var audioConstraint = {
+    mandatory: {
+      chromeMediaSource: 'desktop',
+      chromeMediaSourceId: id,
+    },
+  };
+  console.log(options.canRequestAudioTrack);
+  if (!options.canRequestAudioTrack) audioConstraint = false;
+
+  navigator.webkitGetUserMedia(
+    {
+      audio: audioConstraint,
+      video: {
+        mandatory: {
+          chromeMediaSource: 'desktop',
+          chromeMediaSourceId: id,
+          maxWidth: screen.width,
+          maxHeight: screen.height,
+        },
+      },
+    },
+    gotStream,
+    getUserMediaError
+  );
+}
+
+function getUserMediaError(error) {
+  console.log('navigator.webkitGetUserMedia() errot: ', error);
+}
+function gotStream(stream) {
+  console.log('Received local stream', stream);
+  var views = chrome.extension.getViews({type:'popup'});
+  for (var i=0; i<views.length; i++){
+      var video = views[i].document.getElementById('screenCapture');
+      console.log('video');
+      try {
+        video.srcObject = stream;
+      } catch (error) {
+        console.log(error);
+        video.src = URL.createObjectURL(stream);
+      }
+      stream.onended = function () {
+        console.log('Ended');
+      };
+  }
+}
