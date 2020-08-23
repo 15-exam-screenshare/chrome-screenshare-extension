cd18a9f7f7cd8d71d9dfc1d6e88475fc7892843a
penguin418 <penguin418@naver.com>
2020-08-23 17:23:07 +0900

    doc: Added comments and refactored listeners
    splited listener of startRecord button

--- popup.js
+++ popup.js
@@ -1,16 +1,21 @@
 // message pass 준비
 var port = chrome.extension.connect({
-    name: 'Sample Connection from popup'
+  name: "Sample Connection from popup",
 });
 
 // message pass 수신
-port.onMessage.addListener((msg)=>{
-    console.log('message from backgroun:', msg);
+port.onMessage.addListener((msg) => {
+  console.log("message from backgroun:", msg);
 });
 
-// init
-window.onload = ()=>{
-    document.querySelector('#startRecord').addEventListener('click', (event)=>{
-        port.postMessage({'action': 'startRecord'});
-    });
+/// init
+/* html이 모두 로드되고 실행되는 부분 */
+window.onload = init;
+function init() {
+  /* startRecord 버튼에 대해 리스너가 부여됨 */
+  document.querySelector("#startRecord").addEventListener("click", onClickStartRecord);
 }
+
+function onClickStartRecord(event){
+    port.postMessage({ action: "startRecord" });
+  }
